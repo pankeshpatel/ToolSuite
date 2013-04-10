1b07927538c5e491584c1767d77bf863eb5024fc
soukaras dimitris <tst08002@uop.gr>
2013-04-10 14:23:33 +0200

    1. GUI part bugs are fixed! 2. Library of packages (pubsub,common,semantic) is stored!

--- ANTLR/IBICOOPpubsubmiddleware/Subscriber.java
+++
@@ -1,9 +0,0 @@
-package fr.inria.arles.pankesh.pubsubmiddleware;
-
-import fr.inria.arles.pankesh.semanticmodel.Device;
-
-public interface Subscriber {
-
-	public abstract void notifyReceived(String eventName, Object arg,
-			Device deviceInfo);
-}
